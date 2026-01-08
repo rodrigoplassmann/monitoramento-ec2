e5ed6f4ee20dc53fbf38c061a77e3789d27b4e74
Ubuntu <ubuntu@ip-172-31-2-196.us-east-2.compute.internal>
2026-01-08 20:46:47 +0000

    Arrumando para que o disco seja mostrado em GB

--- agent/collector.py
+++ agent/collector.py
@@ -6,7 +6,7 @@ load_dotenv()
 data = {
     "cpu": psutil.cpu_percent(),
     "ram": psutil.virtual_memory().percent,
-    "disk": psutil.disk_usage("/").percent
+    "disk": psutil.disk_usage("/").used/1024**3
 }
 
 requests.post(os.getenv("API_URL"), json=data)
