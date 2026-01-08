028df372d8591e5df0e8f975468fb25a0ba2f187
Ubuntu <ubuntu@ip-172-31-2-196.us-east-2.compute.internal>
2026-01-08 19:26:55 +0000

    Arquivo python com o coletor das métricas

---
+++ agent/collector.py
@@ -0,0 +1,12 @@
+import psutil, requests, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+data = {
+    "cpu": psutil.cpu_percent(),
+    "ram": psutil.virtual_memory().percent,
+    "disk": psutil.disk_usage("/").percent
+}
+
+requests.post(os.getenv("API_URL"), json=data)
