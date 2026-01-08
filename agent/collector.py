9701b65e24fb789896ec5be60e38e5ce81f2613f
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2026-01-08 22:28:35 +0000

    Modificação na métrica de cpu para resultados mais espaçados

--- agent/collector.py
+++ agent/collector.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 data = {
-    "cpu": psutil.cpu_percent(),
+    "cpu": psutil.cpu_percent(interval=1),
     "ram": psutil.virtual_memory().percent,
     "disk": psutil.disk_usage("/").used/1024**3
 }
